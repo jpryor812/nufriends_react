bfa3c18666a75ccf032445e65b26426d4ae8a972
jpryor812 <85300205+jpryor812@users.noreply.github.com>
2024-09-19 15:49:36 -0400

    Update LandingPage.jsx

--- src/screens/LandingPage/LandingPage.jsx
+++ src/screens/LandingPage/LandingPage.jsx
@@ -6,7 +6,7 @@ export const LandingPage = () => {
     <div className="landing-page">
       <div className="div">
         <div className="overlap">
-          <img className="emoji-waving-hand" alt="Emoji waving hand" src="/img/emoji-waving-hand-sign.svg" />
+          <img className="emoji-waving-hand" alt="Emoji waving hand" src="/emoji-waving-hand-sign.svg" />
           <div className="text-wrapper">Find My Friends</div>
         </div>
         <div className="overlap-group">
@@ -15,9 +15,9 @@ export const LandingPage = () => {
           <div className="group">
             <div className="text-wrapper-3">nufriends</div>
           </div>
-          <img className="img" alt="Emoji waving hand" src="/img/emoji-waving-hand-sign-1.png" />
-          <img className="busts-in-silhouette" alt="Busts in silhouette" src="/img/busts-in-silhouette.svg" />
-          <img className="robot-final" alt="Robot final" src="/img/robot-final-3-1-1.svg" />
+          <img className="img" alt="Emoji waving hand" src="/emoji-waving-hand-sign-1.png" />
+          <img className="busts-in-silhouette" alt="Busts in silhouette" src="/busts-in-silhouette.svg" />
+          <img className="robot-final" alt="Robot final" src="/robot-final-3-1-1.svg" />
         </div>
         <p className="text-wrapper-4">
           Worried about the cold start to the conversation? Your AI Companion, who knows you best, will facilitate that
@@ -27,7 +27,7 @@ export const LandingPage = () => {
           Answer a few questions to help us get to know you, and then we’ll match you with people we feel you’ll build a
           connection with!
         </p>
-        <img className="icon-mail" alt="Icon mail" src="/img/icon-mail.png" />
+        <img className="icon-mail" alt="Icon mail" src="/icon-mail.png" />
       </div>
     </div>
   );
